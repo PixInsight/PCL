3327aa4c5b1522dd2241002b1dc2d945133e0a55
Klaus Kretzschmar <kkretzschmar@gmail.com>
2014-05-17 17:01:31 +0200

    add macosx build

--- run_build.sh
+++ run_build.sh
@@ -10,4 +10,4 @@ fi
 echo $OS_PATH
 export PCLLIBDIR64=../../PCL/lib/$OS_PATH/x64
 echo $PCLLIBDIR64
-#cd $OS_PATH/g++/ && mkdir -p x86_64/Release  && make -f makefile-x86_64
+cd $OS_PATH/g++/ && mkdir -p x86_64/Release  && make -f makefile-x86_64

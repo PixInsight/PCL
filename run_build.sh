e108397c0ce45c7c7ca3778526c753ece07a2753
Klaus Kretzschmar <kkretzschmar@gmail.com>
2014-05-17 16:43:26 +0200

    debuf build script

--- run_build.sh
+++ run_build.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env sh
+echo $TRAVIS_OS_NAME
 if [$TRAVIS_OS_NAME -eq "linux"] 
 then
- OS_PATH="linux"
+ export OS_PATH="linux"
 elif [$TRAVIS_OS_NAME -eq "osx"]
 then
- OS_PATH="macosx"
+ export OS_PATH="macosx"
 fi
+echo $OS_PATH
 export PCLLIBDIR64=../../PCL/lib/$OS_PATH/x64
 cd $OS_PATH/g++/ && mkdir -p x86_64/Release  && make -f makefile-x86_64

01b52fc7b57f520749ab2d5aac4c1980fa00ec0b
Klaus Kretzschmar <kkretzschmar@gmail.com>
2014-05-17 17:08:52 +0200

    roll back macosx build

--- run_build.sh
+++ run_build.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env sh
 echo $TRAVIS_OS_NAME
-if [ "$TRAVIS_OS_NAME" = "linux" ]; 
-then
+#if [ "$TRAVIS_OS_NAME" = "linux" ]; 
+#then
  export OS_PATH="linux"
-elif [ "$TRAVIS_OS_NAME" = "osx" ];
-then
- export OS_PATH="macosx"
-fi
+#elif [ "$TRAVIS_OS_NAME" = "osx" ];
+#then
+# export OS_PATH="macosx"
+#fi
 echo $OS_PATH
 export PCLLIBDIR64=../../PCL/lib/$OS_PATH/x64
 echo $PCLLIBDIR64

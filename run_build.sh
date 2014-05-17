7eb45e48d9f9ec51b482433fc427a3bf3e669c61
Klaus Kretzschmar <kkretzschmar@gmail.com>
2014-05-17 16:51:11 +0200

    fix build script

--- run_build.sh
+++ run_build.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env sh
 echo $TRAVIS_OS_NAME
-if [$TRAVIS_OS_NAME -eq "linux"] 
+if [ "$TRAVIS_OS_NAME" = "linux" ]; 
 then
  export OS_PATH="linux"
-elif [$TRAVIS_OS_NAME -eq "osx"]
+elif [ "$TRAVIS_OS_NAME" = "osx" ];
 then
  export OS_PATH="macosx"
 fi
 echo $OS_PATH
 export PCLLIBDIR64=../../PCL/lib/$OS_PATH/x64
-cd $OS_PATH/g++/ && mkdir -p x86_64/Release  && make -f makefile-x86_64
+echo $PCLLIBDIR64
+#cd $OS_PATH/g++/ && mkdir -p x86_64/Release  && make -f makefile-x86_64

1672b2ee55c6fa2f751203147bfd474632d1561b
jconejero <juan.conejero@pixinsight.com>
2016-04-19 11:54:49 +0200

    INDIClient: Remove PCL license headers from INDI library files (accidentally included by our code maintenance utilities)

--- src/modules/processes/contrib/kkretzschmar/INDIClient/INDI/BaseClientImpl.cpp
+++ src/modules/processes/contrib/kkretzschmar/INDIClient/INDI/BaseClientImpl.cpp
@@ -1,55 +1,3 @@
-//     ____   ______ __
-//    / __ \ / ____// /
-//   / /_/ // /    / /
-//  / ____// /___ / /___   PixInsight Class Library
-// /_/     \____//_____/   PCL 02.01.01.0784
-// ----------------------------------------------------------------------------
-// Standard INDIClient Process Module Version 01.00.04.0108
-// ----------------------------------------------------------------------------
-// BaseClientImpl.cpp - Released 2016/04/15 15:37:39 UTC
-// ----------------------------------------------------------------------------
-// This file is part of the standard INDIClient PixInsight module.
-//
-// Copyright (c) 2014-2016 Klaus Kretzschmar
-//
-// Redistribution and use in both source and binary forms, with or without
-// modification, is permitted provided that the following conditions are met:
-//
-// 1. All redistributions of source code must retain the above copyright
-//    notice, this list of conditions and the following disclaimer.
-//
-// 2. All redistributions in binary form must reproduce the above copyright
-//    notice, this list of conditions and the following disclaimer in the
-//    documentation and/or other materials provided with the distribution.
-//
-// 3. Neither the names "PixInsight" and "Pleiades Astrophoto", nor the names
-//    of their contributors, may be used to endorse or promote products derived
-//    from this software without specific prior written permission. For written
-//    permission, please contact info@pixinsight.com.
-//
-// 4. All products derived from this software, in any form whatsoever, must
-//    reproduce the following acknowledgment in the end-user documentation
-//    and/or other materials provided with the product:
-//
-//    "This product is based on software from the PixInsight project, developed
-//    by Pleiades Astrophoto and its contributors (http://pixinsight.com/)."
-//
-//    Alternatively, if that is where third-party acknowledgments normally
-//    appear, this acknowledgment must be reproduced in the product itself.
-//
-// THIS SOFTWARE IS PROVIDED BY PLEIADES ASTROPHOTO AND ITS CONTRIBUTORS
-// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
-// TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
-// PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL PLEIADES ASTROPHOTO OR ITS
-// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
-// EXEMPLARY OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, BUSINESS
-// INTERRUPTION; PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; AND LOSS OF USE,
-// DATA OR PROFITS) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-// POSSIBILITY OF SUCH DAMAGE.
-// ----------------------------------------------------------------------------
-
 /*******************************************************************************
 
  This library is free software; you can redistribute it and/or
@@ -113,7 +61,7 @@ void INDIListener::Run(){
 }
 
 
-INDI::BaseClientImpl::BaseClientImpl():sConnected(false){}
+INDI::BaseClientImpl::BaseClientImpl(){}
 
 INDI::BaseClientImpl::BaseClientImpl(const char* hostname, unsigned int port)
 {
@@ -911,6 +859,3 @@ void INDI::BaseClientImpl::sendCommand(char *fmt, ...)
 	fprintf(svrwfp,sendbuf);
 #endif
 }
-
-// ----------------------------------------------------------------------------
-// EOF BaseClientImpl.cpp - Released 2016/04/15 15:37:39 UTC

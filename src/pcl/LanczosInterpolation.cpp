67b87848e578a5cf47ee3779bec36cfab63217c0
jconejero <juan.conejero@pixinsight.com>
2016-12-12 23:08:24 +0100

    Improved Lanczos interpolation implementation

--- src/pcl/LanczosInterpolation.cpp
+++ src/pcl/LanczosInterpolation.cpp
@@ -57,9 +57,18 @@ namespace pcl
 
 // ----------------------------------------------------------------------------
 
-PCL_DATA float* PCL_Lanczos3_LUT = nullptr;
-PCL_DATA float* PCL_Lanczos4_LUT = nullptr;
-PCL_DATA float* PCL_Lanczos5_LUT = nullptr;
+/*
+ * Real Lanczos LUT data requires a maximum of about 190 KiB.
+ * Integer Lanczos LUT data requires a maximum of 3 MiB.
+ */
+static double** PCL_Lanczos_LUT[]     = { nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr };
+static float*   PCL_Lanczos_Int_LUT[] = { nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr };
+
+static void ValidateLanczosLUTOrder( int n )
+{
+   if ( n < 1 || n > 8 )
+      throw Error( "Unsupported Lanczos LUT order. Must be 1 <= n <= 8." );
+}
 
 // ----------------------------------------------------------------------------
 
@@ -77,24 +86,60 @@ static double Sinc( double x )
  */
 static double Lanczos( double x, int n )
 {
-   return Sinc( x ) * Sinc( x/n );
+   if ( x < 0 )
+      x = -x;
+   if ( x < n )
+      return Sinc( x ) * Sinc( x/n );
+   return 0;
 }
 
 // ----------------------------------------------------------------------------
 
-// Thread-safe routine
-void PCL_FUNC PCL_InitializeLanczosLUT( float*& LUT, int n )
-{
-   static Mutex mutex;
-   volatile AutoLock lock( mutex );
+/*
+ * Thread-safe LUT initialization routines.
+ */
 
-   if ( LUT == nullptr )
+const double** PCL_FUNC PCL_InitializeLanczosRealLUT( int n )
+{
+   ValidateLanczosLUTOrder( n );
+
    {
-      LUT = new float[ n*__PCL_LANCZOS_LUT_RESOLUTION + 1 ];
-      for ( int i = 0, k = 0; i < n; ++i )
-         for ( int j = 0; j < __PCL_LANCZOS_LUT_RESOLUTION; ++j, ++k )
-            LUT[k] = Lanczos( i + double( j )/__PCL_LANCZOS_LUT_RESOLUTION, n );
-      LUT[n*__PCL_LANCZOS_LUT_RESOLUTION] = 0;
+      static Mutex mutex;
+      volatile AutoLock lock( mutex );
+
+      double**& LUT = PCL_Lanczos_LUT[n-1];
+      if ( LUT == nullptr )
+      {
+         LUT = new double*[ 2*n ];
+         for ( int j = -n + 1, k = 0; j <= n; ++j, ++k )
+         {
+            LUT[k] = new double[ __PCL_LANCZOS_LUT_REAL_RESOLUTION ];
+            for ( int i = 0; i < __PCL_LANCZOS_LUT_REAL_RESOLUTION; ++i )
+               LUT[k][i] = Lanczos( j - double( i )/__PCL_LANCZOS_LUT_REAL_RESOLUTION, n );
+         }
+      }
+      return const_cast<const double**>( reinterpret_cast<double**>( LUT ) );
+   }
+}
+
+const float* PCL_FUNC PCL_InitializeLanczosIntLUT( int n )
+{
+   ValidateLanczosLUTOrder( n );
+
+   {
+      static Mutex mutex;
+      volatile AutoLock lock( mutex );
+
+      float*& LUT = PCL_Lanczos_Int_LUT[n-1];
+      if ( LUT == nullptr )
+      {
+         LUT = new float[ n*__PCL_LANCZOS_LUT_INT_RESOLUTION + 1 ];
+         for ( int i = 0, k = 0; i < n; ++i )
+            for ( int j = 0; j < __PCL_LANCZOS_LUT_INT_RESOLUTION; ++j, ++k )
+               LUT[k] = Lanczos( i + double( j )/__PCL_LANCZOS_LUT_INT_RESOLUTION, n );
+         LUT[n*__PCL_LANCZOS_LUT_INT_RESOLUTION] = 0;
+      }
+      return reinterpret_cast<const float*>( LUT );
    }
 }
 
